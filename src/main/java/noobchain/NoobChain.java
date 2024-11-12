3b1be3f97a5e95b5e6e051800c76d8714dbc3091
ShiroAngel <hoangtuananh.ub.tq@gmail.com>
2024-11-12 10:38:28 +0700

    Update transaction demo

--- src/main/java/noobchain/NoobChain.java
+++ src/main/java/noobchain/NoobChain.java
@@ -55,6 +55,7 @@ public class NoobChain {
 //		Block block3 = new Block(block2.hash);
 //		System.out.println("\nWalletB is Attempting to send funds (20) to WalletA...");
 //		block3.addTransaction(walletB.sendFunds( walletA.publicKey, 20));
+//		addBlock(block3);
 		System.out.println("\nWalletA's balance is: " + walletA.getBalance());
 		System.out.println("WalletB's balance is: " + walletB.getBalance());
 
